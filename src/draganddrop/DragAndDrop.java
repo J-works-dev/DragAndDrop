d6d3f2b64c4c8308c70aa9d56135c9817a8b8bbf
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2021-05-06 11:02:20 +0800

    Change Box to GridPane

--- src/draganddrop/DragAndDrop.java
+++ src/draganddrop/DragAndDrop.java
@@ -9,12 +9,14 @@ package draganddrop;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventTarget;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.*;
 import javafx.scene.paint.Color;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
@@ -28,6 +30,7 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -35,42 +38,41 @@ public class DragAndDrop extends Application {
 
     ArrayList<Text> toDoList = new ArrayList<Text>();
     ArrayList<Text> doneList = new ArrayList<Text>();
-    
+    static int count = 0;
     
     @Override
     public void start(Stage stage) {
         stage.setTitle("J-works To Do List");
         Text first = new Text(50, 100, "Code My Dream!");
+        Text second = new Text(50, 100, "second");
+        Text third = new Text(50, 100, "third!");
         toDoList.add(first);
+        toDoList.add(second);
+        toDoList.add(third);
         
-        SplitPane splitPane = new SplitPane();
+        final SplitPane splitPane = new SplitPane();
         GridPane leftGrid = new GridPane();
         leftGrid.setAlignment(Pos.CENTER);
         leftGrid.setHgap(10);
         leftGrid.setVgap(10);
         leftGrid.setPadding(new Insets(10, 10, 10, 10));
-        
-//        VBox leftControl  = new VBox();
         Label leftTitle = new Label("To Do List");
         leftTitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
         leftGrid.add(leftTitle, 0, 0, 2, 1);
-//        leftTitle.setScaleX(2.0);
-//        leftTitle.setScaleY(2.0);
-//        leftControl.getChildren().add(leftTitle);
-//        leftControl.setAlignment(Pos.TOP_CENTER);
-        
-        final VBox rightControl = new VBox();
+               
+        GridPane rightGrid = new GridPane();
+        rightGrid.setAlignment(Pos.CENTER);
+        rightGrid.setHgap(10);
+        rightGrid.setVgap(10);
+        rightGrid.setPadding(new Insets(10, 10, 10, 10));
         Label rightTitle = new Label("Done List");
-        rightTitle.setScaleX(2.0);
-        rightTitle.setScaleY(2.0);
-        rightControl.getChildren().add(rightTitle);
-        rightControl.setAlignment(Pos.TOP_CENTER);
-        splitPane.getItems().addAll(leftGrid, rightControl);
+        rightTitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 15));
+        rightGrid.add(rightTitle, 0, 0, 2, 1);
+
+        splitPane.getItems().addAll(leftGrid, rightGrid);
 
         Scene scene = new Scene(splitPane, 800, 500);
-//        Group root = new Group();
-//        Scene scene = new Scene(root, 600, 500);
-//        scene.setFill(Color.LIGHTGREEN);
+        scene.setFill(Color.LIGHTGREEN);
 
         TextField getList = new TextField();
         Button buttonAdd = new Button("ADD");
@@ -78,28 +80,61 @@ public class DragAndDrop extends Application {
         buttonAdd.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                String list = getList.getText();
-                System.out.println(list);
-                toDoList.add(new Text(list));
+                if (count > 10) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Alert");
+                    alert.setHeaderText("To Do List is Full!");
+                    alert.setContentText("Add after finish task");
+                    alert.showAndWait();
+                } else {
+                    String list = getList.getText();
+                    toDoList.add(new Text(list));
+                    leftGrid.add(toDoList.get(count), 0, count + 2);
+                    count++;
+                }
             }
         });
         if (toDoList.size() > 0) {
             for (int i = 0; i < toDoList.size(); i++) {
 //                leftControl.getChildren().add(toDolist.get(i));
                 leftGrid.add(toDoList.get(i), 0, i + 2);
+                count++;
             }
         }
-        final Text source = toDoList.get(0);
-//        source = toDoList.get(0);
-        Text target = new Text();
+        
+        Text source = toDoList.get(0);
+//        final Text target;
 
-        source.setOnDragDetected(new EventHandler<MouseEvent>() {
-            @Override
+//        splitPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent mouseEvent) {
+//              System.out.println(mouseEvent.getTarget());
+//              System.out.println(mouseEvent.getTarget() instanceof Text);
+//              System.out.println(mouseEvent.getTarget().getClass().getSimpleName());
+//              if (mouseEvent.getTarget() instanceof Text) {
+////                  Text temp = mouseEvent.getTarget().getText();
+//                    EventTarget eventTarget = mouseEvent.getTarget();
+//                    System.out.println(eventTarget);
+//              }
+//            }
+//        });
+        
+        splitPane.setOnDragDetected(new EventHandler<MouseEvent>() {
+         
             public void handle(MouseEvent event) {
                 /* drag was detected, start drag-and-drop gesture*/
                 System.out.println("onDragDetected");
+                System.out.println(event.getTarget());
+                System.out.println(event.getTarget() instanceof Text);
+                System.out.println(event.getTarget().getClass().getSimpleName());
+                if (event.getTarget() instanceof Text) {
+                      EventTarget eventTarget = event.getTarget();
+                      System.out.println(eventTarget);
+                      System.out.println(toDoList.get(0));
+                }
                 
-//                source = toDoList.get(0); // need to change to clicked text
+//                Text source = toDoList.get(0); // need to change to clicked text
+                System.out.println("onDragDetected");
                 /* allow MOVE transfer mode */
                 Dragboard db = source.startDragAndDrop(TransferMode.MOVE);
 
@@ -112,7 +147,7 @@ public class DragAndDrop extends Application {
             }
         });
 
-        rightControl.setOnDragOver(new EventHandler<DragEvent>() {
+        rightGrid.setOnDragOver(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
                 /* data is dragged over the target */
@@ -120,7 +155,7 @@ public class DragAndDrop extends Application {
 
                 /* accept it only if it is  not dragged from the same node 
                  * and if it has a string data */
-                if (event.getGestureSource() != rightControl
+                if (event.getGestureSource() != rightGrid
                         && event.getDragboard().hasString()) {
                     /* allow for moving */
                     event.acceptTransferModes(TransferMode.MOVE);
@@ -130,13 +165,13 @@ public class DragAndDrop extends Application {
             }
         });
 
-        rightControl.setOnDragEntered(new EventHandler<DragEvent>() {
+        rightGrid.setOnDragEntered(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
                 /* the drag-and-drop gesture entered the target */
                 System.out.println("onDragEntered");
                 /* show to the user that it is an actual gesture target */
-                if (event.getGestureSource() != rightControl
+                if (event.getGestureSource() != rightGrid
                         && event.getDragboard().hasString()) {
 //                    target.setFill(Color.GREEN);
                 }
@@ -145,7 +180,7 @@ public class DragAndDrop extends Application {
             }
         });
 
-        rightControl.setOnDragExited(new EventHandler<DragEvent>() {
+        rightGrid.setOnDragExited(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
                 /* mouse moved away, remove the graphical cues */
@@ -155,7 +190,7 @@ public class DragAndDrop extends Application {
             }
         });
 
-        rightControl.setOnDragDropped(new EventHandler<DragEvent>() {
+        rightGrid.setOnDragDropped(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
                 /* data dropped */
@@ -167,9 +202,9 @@ public class DragAndDrop extends Application {
 //                    target.setText(db.getString());
                     doneList.add(new Text(db.getString()));
                     if (doneList.size() > 0) {
-                        for (int i = 2; i < doneList.size() + 2; i++) {
+                        for (int i = 0; i < doneList.size(); i++) {
             //                leftControl.getChildren().add(toDolist.get(i));
-                            leftGrid.add(doneList.get(i), 0, i, 2, 1);
+                            leftGrid.add(doneList.get(i), 0, i + 2, 2, 1);
                         }
                     }
                     success = true;
@@ -182,7 +217,7 @@ public class DragAndDrop extends Application {
             }
         });
 
-        source.setOnDragDone(new EventHandler<DragEvent>() {
+        leftGrid.setOnDragDone(new EventHandler<DragEvent>() {
             @Override
             public void handle(DragEvent event) {
                 /* the drag-and-drop gesture ended */
@@ -197,16 +232,15 @@ public class DragAndDrop extends Application {
                             itr.remove();
                         }
                     }
+                    System.out.println(toDoList);
                 }
 
                 event.consume();
             }
         });
 
-//        leftGrid.add(source, 0, 2, 2, 1);
-//        leftControl.getChildren().add(source);
-        leftGrid.add(getList, 0, 12, 2, 1);
-        leftGrid.add(buttonAdd, 1, 13);
+        leftGrid.add(getList, 0, 15, 2, 1);
+        leftGrid.add(buttonAdd, 2, 15);
 //        leftControl.getChildren().add(getList);
 //        leftControl.getChildren().add(buttonAdd);
 //        rightControl.getChildren().add(target);
